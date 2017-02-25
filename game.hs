b9e22e6693b8e415ffee8c1c58dcf29ee42a37fe
John Shaw <shaw_fire@yahoo.com>
2017-02-26 05:08:42 +1100

    Report on the number of guesses, and take out trace output.

--- game.hs
+++ game.hs
@@ -1,26 +1,27 @@
 
 import System.Random
 
-tooLow secret_number = do
-  print "Too low... :("
-  game secret_number
+tooLow :: Int -> Int -> IO ()
+tooLow secret_number guesses = do
+  putStrLn "Too low... :("
+  game secret_number $ guesses
 
-tooHigh secret_number = do
-  print "Too high... :("
-  game secret_number
+tooHigh :: Int -> Int -> IO ()
+tooHigh secret_number guesses = do
+  putStrLn "Too high... :("
+  game secret_number $ guesses
 
-game :: Int -> IO ()
-game secret_number = do
-  print "Enter your guess between 1 and 10 ? "
+game :: Int -> Int -> IO ()
+game secret_number guesses = do
+  putStr "Enter your guess between 1 and 10 ? "
   guess <- readLn
   case compare guess secret_number
-    of  GT -> tooHigh secret_number
-        LT -> tooLow secret_number
-        EQ -> print "You win!"
+    of  GT -> tooHigh secret_number $ guesses+1
+        LT -> tooLow secret_number $ guesses+1
+        EQ -> putStrLn $ "You win! after " ++ (show (guesses+1)) ++ " guesses."
 
 main :: IO ()
 main = do
   putStrLn "Let's play the number guessing game"
   secret_number <- randomRIO (1,10)
-  putStrLn ("My secret random number is: "++show secret_number)
-  game secret_number
+  game secret_number 0
