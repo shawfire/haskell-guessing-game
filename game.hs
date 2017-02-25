15b6940aa3226a4ddd0f50be969b9ccd5b6df585
John Shaw <shaw_fire@yahoo.com>
2017-02-26 05:02:20 +1100

    Add feedback to user guess.

--- game.hs
+++ game.hs
@@ -1,8 +1,18 @@
 
 import System.Random
 
+game :: Int -> IO ()
+game secret_number = do
+  print "Enter your guess between 1 and 10 ? "
+  guess <- readLn
+  case compare guess secret_number
+    of  GT -> print "Too high... :("
+        LT -> print "Too low... :("
+        EQ -> print "You win!"
+
 main :: IO ()
 main = do
   putStrLn "Let's play the number guessing game"
-  number <- randomRIO (1,10) :: IO Int
-  putStrLn ("My secret random number is: "++show number)
+  secret_number <- randomRIO (1,10)
+  putStrLn ("My secret random number is: "++show secret_number)
+  game secret_number
