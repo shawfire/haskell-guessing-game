7523cafff5d6728043802e1653582c211bfe00b0
John Shaw <shaw_fire@yahoo.com>
2017-02-27 13:18:45 +1100

    Add you loose after so many attempts.

--- game.hs
+++ game.hs
@@ -1,27 +1,36 @@
 
 import System.Random
 
+max_guesses = 3
+
 tooLow :: Int -> Int -> IO ()
 tooLow secret_number guesses = do
-  putStrLn "Too low... :("
-  game secret_number $ guesses
+  if (guesses >= max_guesses)
+    then putStrLn $ "\nToo low... You Loose after " ++ (show (guesses)) ++ " attempts :(\n"
+    else do
+      putStrLn "Too low... Try higher."
+      game secret_number $ guesses
 
 tooHigh :: Int -> Int -> IO ()
 tooHigh secret_number guesses = do
-  putStrLn "Too high... :("
-  game secret_number $ guesses
+  if (guesses >= max_guesses)
+    then putStrLn $ "\nToo high... You Loose after " ++ (show (guesses)) ++ " attempts :(\n"
+    else do
+      putStrLn "Too high... Try lower."
+      game secret_number $ guesses
 
 game :: Int -> Int -> IO ()
 game secret_number guesses = do
-  putStr "Enter your guess between 1 and 10 ? "
+  putStr "\nEnter your guess between 1 and 10 ? "
   guess <- readLn
   case compare guess secret_number
     of  GT -> tooHigh secret_number $ guesses+1
         LT -> tooLow secret_number $ guesses+1
-        EQ -> putStrLn $ "You win! after " ++ (show (guesses+1)) ++ " guesses."
+        EQ -> putStrLn $ "\nYou win! :) after " ++ (show (guesses+1)) ++ " guess" ++ plural ++ ".\n"
+        where plural = if guesses > 1 then "es" else ""
 
 main :: IO ()
 main = do
-  putStrLn "Let's play the number guessing game"
+  putStrLn "\nLet's play the number guessing game"
   secret_number <- randomRIO (1,10)
   game secret_number 0
